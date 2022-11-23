cb4e6c7ebb24649533ca2c169e13c46800aeefdf
Zhong Ming Tan <31769920+ming0520@users.noreply.github.com>
2022-11-24 00:39:01 +0800

    Add loading screen, loading icon, side bar image, remove report keyword

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:polishop/models/GroceeryCategory.dart';
 import 'package:polishop/screen/AddCategoryScreen.dart';
@@ -7,8 +9,11 @@ import 'package:polishop/screen/CostScreen.dart';
 import 'package:polishop/screen/ListProductScreen.dart';
 import 'package:polishop/screen/ProfitScreen.dart';
 import 'package:polishop/screen/ReportScreen.dart';
+import 'package:polishop/screen/RestockScreen.dart';
 import 'package:polishop/screen/RevenueScreen.dart';
+import 'package:polishop/screen/StockListScreen.dart';
 import 'package:polishop/screen/StockScreen.dart';
+import 'package:polishop/screen/UpdateStockScreen.dart';
 import 'package:polishop/widgets/categoryWidget.dart';
 import 'package:http/http.dart' as http;
 // import 'package:sidebarx/sidebarx.dart';
@@ -37,6 +42,7 @@ class _App extends State<App> {
 
   void populateAllMovies() async {
     final movies = await _fetchAllMovies();
+    sleep(Duration(seconds: 3));
     setState(() {
       loading = false;
       _movies = movies;
@@ -65,30 +71,51 @@ class _App extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: kTITLE, initialRoute: '/', routes: {
-      '/': (context) => mainScreen(movies: _movies),
-      // '/list': (context) => const ListScreen(),
-    });
+    return loading
+        ? Container(
+            color: Colors.blue,
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 150,
+                  ),
+                  Image(
+                    image: AssetImage('assets/ic_launcher.png'),
+                  )
+                ],
+              ),
+            ),
+          )
+        : MaterialApp(title: kTITLE, initialRoute: '/', routes: {
+            '/': (context) => mainScreen(
+                  movies: _movies,
+                  isLoading: loading,
+                ),
+            // '/list': (context) => const ListScreen(),
+          });
   }
 }
 
 class mainScreen extends StatefulWidget {
-  mainScreen({
-    Key? key,
-    required List<GroceeryCategory> movies,
-  })  : myMovieList = movies,
+  mainScreen({Key? key, required List<GroceeryCategory> movies, isLoading})
+      : myMovieList = movies,
         super(key: key);
 
   List<GroceeryCategory> myMovieList;
-
+  bool isLoading = true;
   @override
   State<mainScreen> createState() => _mainScreenState();
 }
 
 class _mainScreenState extends State<mainScreen> {
+  bool loading = false;
+
   void populateAllMovies() async {
+    loading = true;
     final movies = await _fetchAllMovies();
     setState(() {
+      loading = false;
       widget.myMovieList = movies;
     });
   }
@@ -124,6 +151,9 @@ class _mainScreenState extends State<mainScreen> {
             ),
             onPressed: () {
               populateAllMovies();
+              setState(() {
+                loading = true;
+              });
               print('Refreshed');
             },
           )
@@ -137,18 +167,21 @@ class _mainScreenState extends State<mainScreen> {
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: [
-            const DrawerHeader(
+            DrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.blue,
               ),
-              child: Text(
-                "E-Inventory Management System",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28,
-                ),
+              child: Image(
+                image: AssetImage('assets/ic_launcher.png'),
               ),
+              // Text(
+              //   "E-Inventory Management System",
+              //   style: TextStyle(
+              //     color: Colors.white,
+              //     fontWeight: FontWeight.bold,
+              //     fontSize: 28,
+              //   ),
+              // ),
             ),
             // ListTile(
             //   title: const Text(
@@ -164,7 +197,23 @@ class _mainScreenState extends State<mainScreen> {
             // ),
             ListTile(
               title: const Text(
-                'Stock Report',
+                'Manage Stock',
+                style: TextStyle(color: Colors.black87),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => StockListScreen(
+                      category: GroceeryCategory(id: -1, category_name: 'All'),
+                    ),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text(
+                'Stock',
                 style: TextStyle(color: Colors.black87),
               ),
               onTap: () {
@@ -178,7 +227,21 @@ class _mainScreenState extends State<mainScreen> {
             ),
             ListTile(
               title: const Text(
-                'Sales Report',
+                'Restock',
+                style: TextStyle(color: Colors.black87),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RestockScreen(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text(
+                'Sales',
                 style: TextStyle(color: Colors.black87),
               ),
               onTap: () {
@@ -192,7 +255,7 @@ class _mainScreenState extends State<mainScreen> {
             ),
             ListTile(
               title: const Text(
-                'Cost Report',
+                'Cost',
                 style: TextStyle(color: Colors.black87),
               ),
               onTap: () {
@@ -206,7 +269,7 @@ class _mainScreenState extends State<mainScreen> {
             ),
             ListTile(
               title: const Text(
-                'Profit Report',
+                'Profit',
                 style: TextStyle(color: Colors.black87),
               ),
               onTap: () {
@@ -221,7 +284,7 @@ class _mainScreenState extends State<mainScreen> {
 
             ListTile(
               title: const Text(
-                'All Report',
+                'Report',
                 style: TextStyle(color: Colors.black87),
               ),
               onTap: () {
@@ -236,72 +299,88 @@ class _mainScreenState extends State<mainScreen> {
           ],
         ),
       ),
-      body: ListView.builder(
-          itemCount: widget.myMovieList.length,
-          itemBuilder: (context, index) {
-            final movie = widget.myMovieList[index];
+      body: loading
+          ? Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 150,
+                  ),
+                  CircularProgressIndicator(),
+                  Image(
+                    image: AssetImage('assets/ic_launcher.png'),
+                  )
+                ],
+              ),
+            )
+          : ListView.builder(
+              itemCount: widget.myMovieList.length,
+              itemBuilder: (context, index) {
+                final movie = widget.myMovieList[index];
 
-            return ListTile(
-                title: Row(
-              children: [
-                // SizedBox(
-                //     width: 100,
-                //     child: ClipRRect(
-                //       child: Image.network(movie.poster),
-                //       borderRadius: BorderRadius.circular(10),
-                //     )
-                //
-                // ),
-                Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.all(0.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          title: GestureDetector(
-                            child: Container(
-                              child: Text(
-                                index.toString() + ". " + movie.category_name,
+                return ListTile(
+                    title: Row(
+                  children: [
+                    // SizedBox(
+                    //     width: 100,
+                    //     child: ClipRRect(
+                    //       child: Image.network(movie.poster),
+                    //       borderRadius: BorderRadius.circular(10),
+                    //     )
+                    //
+                    // ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(0.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ListTile(
+                              title: GestureDetector(
+                                child: Container(
+                                  child: Text(
+                                    index.toString() +
+                                        ". " +
+                                        movie.category_name,
+                                  ),
+                                ),
+                                onTap: () {
+                                  print(movie.category_name + ' Clicked!');
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          ListScreen(category: movie),
+                                    ),
+                                  );
+                                },
+                              ),
+                              trailing: GestureDetector(
+                                child: Icon(Icons.delete_forever),
+                                onTap: () {
+                                  print('Delete ' + movie.category_name);
+                                  movie.delete();
+                                  setState(() {
+                                    populateAllMovies();
+                                  });
+                                },
                               ),
                             ),
-                            onTap: () {
-                              print(movie.category_name + ' Clicked!');
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      ListScreen(category: movie),
-                                ),
-                              );
-                            },
-                          ),
-                          trailing: GestureDetector(
-                            child: Icon(Icons.delete_forever),
-                            onTap: () {
-                              print('Delete ' + movie.category_name);
-                              movie.delete();
-                              setState(() {
-                                populateAllMovies();
-                              });
-                            },
-                          ),
+                            const Divider(
+                              // height: 20,
+                              // thickness: 3,
+                              // indent: 20,
+                              // endIndent: 0,
+                              color: Colors.black54,
+                            ),
+                            // Text(movie.category_name)
+                          ],
                         ),
-                        const Divider(
-                          // height: 20,
-                          // thickness: 3,
-                          // indent: 20,
-                          // endIndent: 0,
-                          color: Colors.black54,
-                        ),
-                        // Text(movie.category_name)
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ));
-          }),
+                      ),
+                    )
+                  ],
+                ));
+              }),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await Navigator.push(
